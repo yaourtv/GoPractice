b4e784501a576d5bb6ce739480c7cbc5c6cbbeda
Yaourt Ongit <yaourt.ongit@gmail.com>
2020-04-14 01:11:53 +0300

    Add sorting algorithm, change variable name, replace comments with the useful ones

--- Lection2/task1.go
+++ Lection2/task1.go
@@ -3,17 +3,32 @@ package main
 import "fmt"
 
 func median(i []int) float64 {
-	// Defining one extra variable > calling array length 3 times
-	var arrLength = len(i) / 2
+	var pivot = len(i) / 2
 
+	bubbleSort(&i)
 	if len(i)%2 == 1 {
-		return float64(i[arrLength])
-	} else {
-		return float64(i[arrLength]+i[arrLength-1]) / 2
+		return float64(i[pivot])
+	}
+
+	return float64(i[pivot]+i[pivot-1]) / 2
+}
+
+func bubbleSort(arr *[]int) {
+	var arrLength = len(*arr)
+
+	for i := 0; i < arrLength; i++ {
+		for j := 0; j < arrLength-i-1; j++ {
+
+			if (*arr)[j] > (*arr)[j+1] {
+				(*arr)[j], (*arr)[j+1] = (*arr)[j+1], (*arr)[j]
+			}
+		}
 	}
 }
 
 func main() {
-	var i = []int{1, 2, 3, 4, 5}
+	var i = []int{5, 4, 1, 3, 2}
+	var j = []int{7, 1, 5, 4, 3, 6}
 	fmt.Println(median(i))
+	fmt.Println(median(j))
 }
--- Lection2/task2.go
+++ Lection2/task2.go
@@ -2,24 +2,29 @@ package main
 
 import "fmt"
 
+// Structure describes square start point (x, y)
 type Point struct {
 	x, y int
 }
 
+// Structure describes square with its start point and length
 type Square struct {
 	start Point
+	// Square length value
 	a     uint
 }
 
-// We don't want to change start point. Are we?
+// Method takes length and returns square end point
 func (s Square) End() (int, int) {
 	return s.start.x + int(s.a), s.start.y + int(s.a)
 }
 
+// Method takes length and returns square perimeter
 func (s Square) Perimeter() uint {
 	return s.a * 4
 }
 
+// Method takes length and returns square area
 func (s Square) Area() uint {
 	return s.a * s.a
 }
